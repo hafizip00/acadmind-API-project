33f50114865d7dd58b2dc9ff06325f1618e73efd
hafizip00 <hafizip00@gmail.com>
2023-01-11 18:42:11 +0500

    Project

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,37 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+
+import fs from 'fs'
+import path from 'path'
+
+function getPath() {
+  return path.join(process.cwd(), "data", "feedback.json")
 }
+
+function handler(req, res) {
+  if (req.method === "GET") {
+    const fileData = fs.readFileSync(getPath());
+    const data = JSON.parse(fileData)
+    res.status(201).json({ Feedbacks: data })
+  } else if (req.method === "POST") {
+    const { email, feedBack } = req.body
+
+    const feedBackNew = {
+      id: new Date().toISOString(),
+      email: email,
+      feedBack: feedBack
+    }
+    const absPath = getPath()
+    const fileData = fs.readFileSync(absPath)
+    const data = JSON.parse(fileData)
+    data.push(feedBackNew);
+    fs.writeFileSync(absPath, JSON.stringify(data))
+    res.status(200).json({ mesasge: "Success", feedBack: feedBackNew })
+
+  }
+  else {
+    res.status(201).json({ Feedbacks: "Invalid" })
+  }
+}
+
+export default handler
